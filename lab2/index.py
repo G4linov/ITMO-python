a4e78aea6a3e62d34e43829edb2af0ddaa325022
Vyacheslav Galinov <galin0ff@yandex.ru>
2023-10-08 11:13:22 +0300

    Выполнил вторую лр

---
+++ lab2/index.py
@@ -0,0 +1,27 @@
+print("Задача 1.\n")
+
+n = int(input("Введтие n: "))
+m = int(input("Введите m: "))
+
+if (n < m):
+    for i in range(n, m):
+        print(i)
+
+print("Задача 2.\n")
+
+n = int(input("Введтие n: "))
+
+if (n >= 0):
+    for i in range(n, n+10):
+        print(i)
+
+print("Задача 3.\n")
+
+n= int(input("Введтие n: "))
+
+if (n > 0):
+    print("Число не равно или меньше 0")
+elif (n < 0):
+    print("Число не равно или больше 0")
+else:
+    print("Число не имеет знака")
