a4e13d9e3b50049e2fa115efd6bab47f8d630a6b
Vyacheslav Galinov <galin0ff@yandex.ru>
2023-10-29 20:10:40 +0300

    done

--- project/index.py
+++ project/index.py
@@ -1,66 +1,35 @@
-def find_garbage_id(item, garbage_list):
-    for i in range(len(garbage_list)):
-        if (garbage_list[i][0] != -1 ) and (garbage_list[i][1] == item):
-            return i
-    return None
-
-def add_new_garbage(item, garbage_list):
-    id = find_garbage_id(round(item[1], 1), garbage_list)
-    if id is not None:
-        garbage_list[id][0] += item[0]
-    else:
-        garbage_list.append(item)
-    return garbage_list
-
 garbage = []
 out_list = []
 
 iron_len = 11700
 index_order = 0
 
-control = True
-
-while control:
+while True:
     value = int(input("Введите количество эл: "))
     size = int(input("Введите размер эл: "))
 
     out_list.append([index_order + 1, 0, 0])
 
+    index = 0
+    list_of_indexes = []
     for el in garbage:
-        if value >= el[0] * (el[1] // size):
-            temp_el = el.copy()
-            while el[0] >= 1:
-                while el[1] - size > 0:
-                    out_list[index_order][1] += 1
-                    el[1] -= size
-                if el[0] == 1 and el[0] == 0:
-                    el[0], el[1] = -1, -1
-                    break
-                elif el[0] == 1:
-                    el[0] = round(temp_el[0], 1)
-                    break
-                else:
-                    el[0] -= 1
-                    el[1] = temp_el[1]
-        else:
-            new_gabage_el = [0, 0]
-            temp_el = el.copy()
-            while el[1] - size > 0:
+        if (el - size) >= 0 and (out_list[index_order][1] != value):
+            if (el - size) == 0:
+                list_of_indexes.append(index)
                 out_list[index_order][1] += 1
-                el[1] -= size
-                if el[1] - size < 0 and new_gabage_el[0] == 0:
-                    new_gabage_el[0] = 1
-                    new_gabage_el[1] = el[1]
-                    el[0] -= 1
-                    el[1] = temp_el[1]
-                elif el[1] - size < 0:
-                    new_gabage_el[0] += 1
-                    el[0] -= 1
-                    el[1] = temp_el[1]
-                if out_list[index_order][1] == value:
-                    break
-
+            else:
+                el -= size
+                out_list[index_order][1] += 1
+        elif out_list[index_order][1] == value:
+            break
+        index += 1
     
+    for ind in list_of_indexes:
+        garbage[ind] = 0
+
+    while 0 in garbage:
+        garbage.remove(0)
+
     while (out_list[index_order][1] != value):
         temp_iron_len = iron_len
         out_list[index_order][2] += 1
@@ -69,18 +38,13 @@ while control:
             out_list[index_order][1] += 1
             if out_list[index_order][1] == value:
                 break
-        if round(temp_iron_len, 1) != 0:
-            id = find_garbage_id(round(temp_iron_len, 1), garbage)
-            if id is not None:
-                garbage[id][0] += 1
-            else:
-                garbage.append([1, round(temp_iron_len, 1)])
+        garbage.append(temp_iron_len)
             
     index_order += 1
 
     quit_control = input("Для выхода введите 0: ")
     if quit_control == "0":
-        control = False                
+        break              
 
 value_iron = 0
 for el in out_list:
@@ -89,8 +53,9 @@ print("Всего новых железяк:", value_iron)
 
 for el in out_list:
     print("Для заказа №", el[0], "потребуется новых железяк:", el[2])
-
-print("Элементы не использованные:")
+ 
+sum_garbage = 0
 for el in garbage:
-    if el is not None:
-        print("Колчиство:", el[0], "длиной:", el[1])
+    sum_garbage += el
+
+print("Итоговоая длина мусора:", sum_garbage)
--- project/netest.py
+++
@@ -1,7 +0,0 @@
-new = [1, 2]
-my_new = [0, 0]
-my_new[1] = new[1]
-print(my_new)
-new[1] -= 1
-print(new)
-print(my_new)
